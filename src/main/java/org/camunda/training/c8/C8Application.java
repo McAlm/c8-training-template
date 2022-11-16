e0d84f2713282cc0c431f7cc7c36afc7d35591ed
McAlm <stefan.wiese@camunda.com>
2022-11-16 16:51:19 +0100

    exercise 5

--- src/main/java/org/camunda/training/c8/C8Application.java
+++ src/main/java/org/camunda/training/c8/C8Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 
 @SpringBootApplication
 @EnableZeebeClient
+@Deployment(resources = "classpath:bpmn/payment.bpmn")
 public class C8Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/camunda/training/c8/worker/ChargeCreditCardWorker.java
@@ -0,0 +1,19 @@
+package org.camunda.training.c8.worker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+
+@Component
+public class ChargeCreditCardWorker {
+
+    Logger LOG = LoggerFactory.getLogger(ChargeCreditCardWorker.class);
+
+    @JobWorker(type = "chargeCreditCard", autoComplete = true)
+    public void deductCredit() {
+        LOG.info("charging credit card...");
+    }
+
+}

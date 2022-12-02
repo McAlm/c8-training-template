0a864f2709aa9759c7176215794ccd91ebcf2c2a
McAlm <stefan.wiese@camunda.com>
2022-12-02 16:18:28 +0100

    exercise10

--- src/main/java/org/camunda/training/c8/worker/InvokePaymentWorker.java
+++ src/main/java/org/camunda/training/c8/worker/InvokePaymentWorker.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
-import io.camunda.zeebe.spring.client.annotation.Variable;
 import io.camunda.zeebe.spring.client.annotation.VariablesAsType;
 
 @Component
